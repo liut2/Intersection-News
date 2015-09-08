e7ecbbdfd50150af7614ab1289057901ddd4fc20
Tao Liu <liut2@carleton.edu>
2015-09-09 03:15:49 +0800

    added the Product Hunt news and changed the favourite news style

---
+++ ParseStarterProject/PHStoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  PHStoryCell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Tao Liu on 9/8/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class PHStoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
