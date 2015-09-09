6f1087631553c3f931748049b897fda363628b7b
Tao Liu <liut2@carleton.edu>
2015-09-09 22:59:48 +0800

    added the designer news source

---
+++ ParseStarterProject/DNStoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  DNStoryCell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Tao Liu on 9/9/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class DNStoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
